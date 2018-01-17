93fa1b1a7b89ea38e9731474ea9048f7e6551da4
Адель Акимбаева < >
2018-01-17 16:47:37 +0600

    index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 //hey
+//hey2
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
